1cc73918066a414110b612b790d6bec1c75f260b
Gael de Chalendar <gael.de-chalendar@cea.fr>
2019-06-21 12:18:12 +0200

    Rename the Lima class to LimaClient
    Also document its API.

--- aymara/lima.py
+++ aymara/lima.py
@@ -6,16 +6,54 @@ import pyconll
 import requests
 import sys
 
-class Lima:
+class LimaClient:
     """
-    A simple wrapper around the LIMA REST server
+    A simple client for the LIMA REST server
+
+    Attributes
+    ----------
+    host : str
+        the host where the LIMA server is listening
+    port : int
+        the port on which the LIMA server is listening
+
+    Methods
+    -------
+    analyzeText(text, lang='eng', pipeline='deep')
+        Analyze text in lang with the given pipeline
     """
 
     def __init__(self, host='localhost', port='8080'):
+        """
+        Parameters
+        ----------
+        host : str
+            the host where the LIMA server is listening
+        port : int
+            the port on which the LIMA server is listening
+        """
         self.host = host
         self.port = port
 
     def  analyzeText(self, text, lang='eng', pipeline='deep'):
+        """
+        Analyze text in language lang with the given pipeline.
+
+        Parameters
+        ----------
+        text : str
+            The text to analyze (default is None)
+        lang : str, optional
+            The language of the text (default is 'eng')
+        text : str, optional
+            The pipeline to use to analyze the text (default is 'deep')
+
+        Returns
+        -------
+        pyconll.unit.conll.Conll
+            A representation in CoNLL-U format of the text. None if an
+            error occur.
+        """
         url = 'http://{}:{}/?lang={}&pipeline={}'.format(self.host,
                                                          self.port,
                                                          lang,
